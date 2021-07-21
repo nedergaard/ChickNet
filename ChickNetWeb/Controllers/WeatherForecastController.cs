88ae2fbe94421f9957965362cce98de5837ebf8e
Nedergaard <leander.jensen@gmail.com>
2021-07-21 16:02:52 +0200

    Addedtemporary  testing code

--- ChickNetWeb/Controllers/WeatherForecastController.cs
+++ ChickNetWeb/Controllers/WeatherForecastController.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Device.Gpio;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChickNetWeb.Controllers
@@ -13,7 +15,8 @@ namespace ChickNetWeb.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            //"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Varmt", "Køligt", "Lummert",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -26,6 +29,25 @@ namespace ChickNetWeb.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            const int LedPinNumber = 24;
+            const int ButtonPinNumber = 4;
+
+            GpioController controller = new GpioController(PinNumberingScheme.Logical);
+
+            controller.OpenPin(ButtonPinNumber, PinMode.InputPullUp);
+            controller.OpenPin(LedPinNumber, PinMode.Output);
+
+            var blinkCount = 0;
+            bool heartBeat = false;
+            while (controller.Read(ButtonPinNumber) == PinValue.High
+                && blinkCount++ < 30)
+            {
+                heartBeat = !heartBeat;
+                controller.Write(LedPinNumber, heartBeat ? PinValue.High : PinValue.Low);
+                Thread.Sleep(500);
+            }
+
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- ChickNetWeb/Program.cs
+++ ChickNetWeb/Program.cs
@@ -2,10 +2,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
+using System.Device.Gpio;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ChickNetWeb
 {
