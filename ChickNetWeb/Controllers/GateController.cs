883ffa02f00e3f1f0d6c6e7c90db462d313aaa7a
Nedergaard <leander.jensen@gmail.com>
2021-07-26 08:11:33 +0200

    Implemented POC (web-) GateController

---
+++ ChickNetWeb/Controllers/GateController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ChickNetWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GateController : ControllerBase
+    {
+        private readonly Gate.GateController _gateController;
+
+        public GateController(ChickNetApp chickNetService)
+        {
+            _gateController = chickNetService.GateController;
+        }
+
+        // TODO : validate parameters
+        [HttpPost("{id:int}")]
+        public async Task<ActionResult<int>> DesiredState(int id, [FromBody] DesiredStateRequest desiredGateState)
+        {
+            // TODO : Fix Quick & Dirty input validation 
+            if (id < 1 || id > 2 || desiredGateState.State == GateState.Undefined)
+            {
+                return new BadRequestResult();
+            }
+
+            bool isGateDesiredToBeOpen = desiredGateState.State == GateState.Open;
+            if (isGateDesiredToBeOpen)
+            {
+                await _gateController.OpenGateAsync(id);
+            }
+            else
+            {
+                await _gateController.CloseGateAsync(id);
+            }
+
+            return id;
+        }
+    }
+
+    public class DesiredStateRequest
+    {
+        public GateState State { get; set; }
+    }
+
+    public enum GateState
+    {
+        Undefined,
+        Open,
+        Closed,
+    }
+}
